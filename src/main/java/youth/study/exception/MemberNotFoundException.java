ab2c8aaeef514f9642d22a35f058ad78b348cb18
yunhacandy <yunhacandy@g.hongik.ac.kr>
2023-12-17 00:47:18 +0900

    refactor: MemberNotFoundException 코드 변경

--- src/main/java/youth/study/exception/MemberNotFoundException.java
+++ src/main/java/youth/study/exception/MemberNotFoundException.java
@@ -2,7 +2,6 @@ package youth.study.exception;
 
 public class MemberNotFoundException extends RuntimeException {
 
-    public MemberNotFoundException() {  }
     public MemberNotFoundException(String message) {
         super(message);
     }
